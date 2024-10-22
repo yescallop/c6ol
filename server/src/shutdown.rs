1a56c47bd9574e67e467e34e9a740f2603bb3af1
yescallop <yescallop@gmail.com>
2024-10-22 10:08:31 +0800

    Just use a `watch` channel for graceful shutdown
    It's not worth it to use only primitives for this purpose

--- server/src/shutdown.rs
+++ server/src/shutdown.rs
@@ -1,39 +1,25 @@
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
-};
-use tokio::sync::Notify;
+use tokio::sync::watch;
 
-struct Shared {
-    notify: Notify,
-    requested: AtomicBool,
-}
-
-pub struct Sender(Arc<Shared>);
+pub struct Sender(watch::Sender<()>);
 
 impl Sender {
+    /// Requests a shutdown.
     pub fn request(&self) {
-        self.0.requested.store(true, Ordering::Relaxed);
-        self.0.notify.notify_waiters();
+        self.0.send_replace(());
     }
 }
 
 #[derive(Clone)]
-pub struct Receiver(Arc<Shared>);
+pub struct Receiver(watch::Receiver<()>);
 
 impl Receiver {
-    pub async fn requested(self) {
-        let notified = self.0.notify.notified();
-        if !self.0.requested.load(Ordering::Relaxed) {
-            notified.await;
-        }
+    /// Waits unless or until a shutdown is requested or the sender is dropped.
+    pub async fn requested(mut self) {
+        let _ = self.0.changed().await;
     }
 }
 
 pub fn channel() -> (Sender, Receiver) {
-    let shared = Arc::new(Shared {
-        notify: Notify::new(),
-        requested: AtomicBool::new(false),
-    });
-    (Sender(shared.clone()), Receiver(shared))
+    let (tx, rx) = watch::channel(());
+    (Sender(tx), Receiver(rx))
 }
