12ef7051f64ac8e23006885d81a509d1e4c6104d
yescallop <yescallop@gmail.com>
2025-12-18 21:18:34 +0800

    Add sample records

--- core/tests/convert_records.rs
+++ core/tests/convert_records.rs
@@ -48,7 +48,7 @@ fn convert_records() {
         total_bytes_new as f64 / total_moves as f64,
     );
 
-    assert_eq!(total_moves, 754);
-    assert_eq!(total_bytes, 1725);
-    assert_eq!(total_bytes_new, 1203);
+    assert_eq!(total_moves, 1236);
+    assert_eq!(total_bytes, 2839);
+    assert_eq!(total_bytes_new, 2046);
 }
